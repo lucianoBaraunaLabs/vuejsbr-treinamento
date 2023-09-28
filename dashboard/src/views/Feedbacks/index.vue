87732a17aebfe62cd6ce75457b4a12600919919d
Luciano Baraúna <lucianobaraunadev@gmail.com>
2023-09-27 23:26:36 -0300

    modulo-04: finalizada aula Criando tela inicial (Parte 1)

---
+++ dashboard/src/views/Feedbacks/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Aqui é a feedback</h1>
+</template>
+<script>
+export default {
+
+}
+</script>
---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="text-brand-main">Aqui é a home</h1>
+</template>
+<script>
+export default {
+
+  }
+</script>
