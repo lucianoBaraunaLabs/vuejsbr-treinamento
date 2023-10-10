d2751ab0cf92c984b1a3e1954e1a7ab51acfe995
Luciano Baraúna <lucianobaraunadev@gmail.com>
2023-10-10 18:17:51 -0300

    modulo-04: finzalida criando modal login-1

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -1,13 +1,40 @@
-<template lang="">
-  <div>
-    componente de modal login
+<template>
+  <div class="flex justify-between" id="modal-login">
+    <h1 class="text-4xl font-black text-gray-800">
+      Entre na sua conta
+    </h1>
+
+    <button
+      @click="close"
+      class="text-4xl text-gray-600 focus:outline-none"
+    >
+      &times;
+    </button>
   </div>
 </template>
 <script>
-export default {
+import { reactive } from 'vue';
+import useModal from '../../hooks/useModal';
 
+export default {
+  name: 'ModalLogin',
+
+  setup() {
+    const modal = useModal()
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: ''
+      },
+      password: {
+        value: '',
+        errorMessage: ''
+      }
+    })
+
+    return { state, close: modal.close }
+  }
 }
 </script>
-<style lang="">
-
-</style>
