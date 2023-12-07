553131c38ff9d8ebb86bb511e9eb76b364a13874
Luciano Baraúna <lucianobaraunadev@gmail.com>
2023-12-07 17:10:20 -0300

    modulo-04: finalizada - criando tela de feedbacks parte 5

--- dashboard/src/components/FeedbackCard/index.vue
+++ dashboard/src/components/FeedbackCard/index.vue
@@ -1,24 +1,55 @@
 <template>
-  <div
-    @click="handleToggle"
-    class="flex flex-col px-8 py-6 rounded cursor-pointer bg-brand-gray">
+  <div @click="handleToggle" class="flex flex-col px-8 py-6 rounded cursor-pointer bg-brand-gray">
     <div class="flex items-center justify-between w-full mb-8">
       <badge :type="feedback.type" />
+
       <span class="font-regular text-brand-graydark">
-        {{ feedback.createAt }}
+        {{ getDiffTimeBetweenCurrentDate(feedback.createdAt) }}
       </span>
+    </div>
+
+    <div class="text-lg font-medium text-gray-800">
+      {{ feedback.text }}
+    </div>
+
+    <div :class="{
+      animate__fadeOutUp: state.isClosing
+    }" class="flex mt-8 animate__animated animate__fadeInUp animate__faster" v-if="state.isOpen">
+      <div class="flex flex-col w-1/2">
+        <div class="flex flex-col">
+          <span class="font-bold text-gray-400 uppercase select-none">Página</span>
+          <span class="font-medium text-gray-700">{{ feedback.page }}</span>
+        </div>
+        <div class="flex flex-col">
+          <span class="font-bold text-gray-400 uppercase select-none">Dispositivo</span>
+          <span class="font-medium text-gray-700">{{ feedback.device }}</span>
+        </div>
+      </div>
+
+      <div class="flex flex-col w-1/2">
+        <div class="flex flex-col">
+          <span class="font-bold text-gray-400 uppercase select-none">Usuário</span>
+          <span class="font-medium text-gray-700">{{ feedback.fingerprint }}</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-end mt-8" v-if="!state.isOpen">
+      <icon name="chevron-down" size="24" :color="brandColors" />
     </div>
 
   </div>
 </template>
+
 <script>
-import { reactive } from "vue";
+import Icon from '../Icon/index.vue'
 import Badge from './Badge.vue'
+import { getDiffTimeBetweenCurrentDate } from '../../utils/date'
+import { reactive } from 'vue'
+import { wait } from '../../utils/timeout'
+
 export default {
-  name: "FeedbackCard",
-   components: {
-    Badge,
-  },
+  components: { Badge, Icon },
   props: {
     isOpened: { type: Boolean, default: false },
     feedback: { type: Object, required: true }
@@ -29,18 +60,20 @@ export default {
       isClosing: !props.isOpened
     })
 
-    function handleToggle () {
+    async function handleToggle () {
+      state.isClosing = true
 
+      await wait(250)
+      state.isOpen = !state.isOpen
+      state.isClosing = false
     }
 
     return {
       state,
-      handleToggle
+      handleToggle,
+      getDiffTimeBetweenCurrentDate,
+      brandColors: '#afafaf'
     }
   }
-
 }
 </script>
-<style>
-
-</style>
