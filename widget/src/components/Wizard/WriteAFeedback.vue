2400fd769351332d8c60e5ead35285ce7dfd52a3
Luciano Baraúna <lucianobaraunadev@gmail.com>
2023-12-14 18:19:28 -0300

    fazendo aCriando estado de escrever feedback do Wizard

--- widget/src/components/Wizard/WriteAFeedback.vue
+++ widget/src/components/Wizard/WriteAFeedback.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Write feedback</h1>
+    <div class="felx flex-col items-center justify-center w-full my-5">
+      <textarea
+        v-model="state.feedback"
+        class="w-full rounded-lg border-2 border-gray-300 border-solid h-24 p-2 resize-none focus:outline-none">
+    </textarea>
+    <button
+        :disabled="submitButtonIsDisabled"
+        :class="{
+          'opacity-50': state.isLoading,
+          'opacity-50 bg-gray-100 text-gray-500': submitButtonIsDisabled,
+          'bg-brand-main text-white': !submitButtonIsDisabled
+        }"
+        @click="submitAFeedback"
+        class="
+        rounded-lg font-black mt-3 flex flex-col
+        justify-center items-center py-2 w-full cursor-pointer
+        focus:outline-none transition-all duration-200
+      ">
+       <icon v-if="state.isLoading" name="loading" class="animate-spin" color="white" />
+        <template v-else>
+          Enviar feedback
+        </template>
+    </button>
+    </div>
 </template>
 <script lang="ts">
 import useNavigation from '@/hooks/navigation';
 import useStore from '@/hooks/store';
+import services from '@/services';
 import { defineComponent, reactive, computed } from 'vue';
 import type { ComputedRef, SetupContext } from 'vue';
 
@@ -37,6 +61,16 @@ export default defineComponent({
       state.isLoading = false
       setErrorState()
      }
+
+    async function submitAFeedback(): Promise<void>{
+      try {
+        const response = await services.feedbacks.create({
+
+        })
+      } catch (error) {
+
+      }
+    }
 
     return {
       state,
