b3bc5f0595f7ef49907e44b013db39ecc7275f9a
Luciano Baraúna <lucianobaraunadev@gmail.com>
2023-12-26 20:20:19 -0300

    curso finalizado!

--- widget/src/views/Widget/index.vue
+++ widget/src/views/Widget/index.vue
@@ -3,48 +3,52 @@
     <component @open-box="handleOpenBox" @close-box="handleCloseBox" :is="state.component" />
   </teleport>
 </template>
+
 <script lang="ts">
-import { defineComponent, onMounted, reactive } from 'vue'
+import { defineComponent, reactive, watch } from 'vue'
 import Standby from './Standby.vue'
 import Box from './Box.vue'
 import useIframeControl from '../../hooks/iframe'
+import useStore from '../../hooks/store'
 
 type State = {
-  component: string
+  component: string;
 }
 
 interface SetupReturn {
   state: State;
-  handleOpenBox: () => void;
-  handleCloseBox: () => void
+  handleOpenBox(): void;
+  handleCloseBox(): void;
 }
 
-export default defineComponent(  {
-  components: {
-    Standby,
-    Box
-  },
+export default defineComponent({
+  components: { Standby, Box },
   setup(): SetupReturn {
+    const store = useStore()
     const iframe = useIframeControl()
     const state = reactive<State>({
-        component: 'Standby',
+      component: 'Standby'
     })
 
-    onMounted(() => {
+    watch(() => store.currentComponent, () => {
       iframe.updateCoreValuesOnStore()
     })
 
-    function handleOpenBox() {
+    function handleOpenBox(): void {
       iframe.notifyOpen()
       state.component = 'Box'
     }
 
-    function handleCloseBox() {
+    function handleCloseBox(): void {
       iframe.notifyClose()
       state.component = 'Standby'
     }
 
-    return { state, handleOpenBox, handleCloseBox }
-  },
+    return {
+      state,
+      handleOpenBox,
+      handleCloseBox
+    }
+  }
 })
 </script>
