fde0423be1a2f2c4e6556239dea7db907539defb
Luciano Baraúna <lucianobaraunadev@gmail.com>
2023-12-26 18:49:19 -0300

    modulo-07: finalizada - Criando estado de erro do Wizard

---
+++ widget/src/components/Wizard/Error.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex flex-col items-center justify-between w-full my-5">
+    <icon name="atention" color="#EF4983" size="70" />
+
+    <p class="text-xl font-black text-center w-full mt-2">
+      Droga! Aconteceu algum erro.
+    </p>
+
+    <div class="flex justify-center items-center w-full mt-2">
+      <button @click="goBack" class="
+          rounded-full font-regular text-sm flex flex-col justify-center bg-brand-gray
+          items-center py-2 px-5 cursor-pointer focus:outline-none
+        ">
+        Tente novamente
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { resetStore } from '../../store'
+import Icon from '../Icon/index.vue'
+
+interface SetupReturn {
+  goBack(): void;
+}
+
+export default defineComponent({
+  components: { Icon },
+  setup(): SetupReturn {
+    function goBack(): void {
+      resetStore()
+    }
+
+    return {
+      goBack
+    }
+  }
+})
+</script>
