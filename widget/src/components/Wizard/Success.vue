7273d755d71f431ba33fa1a8babf7a99c136404e
Luciano Baraúna <lucianobaraunadev@gmail.com>
2023-12-26 18:48:59 -0300

    modulo-07: finalizada - Criando estado de sucesso do Wizard

--- widget/src/components/Wizard/Success.vue
+++ widget/src/components/Wizard/Success.vue
@@ -1,11 +1,42 @@
 <template>
-  <h1>Success</h1>
+  <div class="flex flex-col items-center justify-between w-full my-5">
+    <icon name="check" color="#EF4983" size="70" />
+
+    <p class="text-xl font-black text-center w-full mt-2">
+      Obrigado! Recebemos o seu feedback.
+    </p>
+
+    <div class="flex justify-center items-center w-full mt-2">
+      <button @click="goBack" class="
+          rounded-full font-regular text-sm flex flex-col justify-center bg-brand-gray
+          items-center py-2 px-5 cursor-pointer focus:outline-none
+        ">
+        Envie mais feedbacks
+      </button>
+    </div>
+
+  </div>
 </template>
+
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue'
+import { resetStore } from '../../store'
+import Icon from '../Icon/index.vue'
 
+interface SetupReturn {
+  goBack(): void;
 }
-</script>
-<style lang="">
 
-</style>
+export default defineComponent({
+  components: { Icon },
+  setup(): SetupReturn {
+    function goBack(): void {
+      resetStore()
+    }
+
+    return {
+      goBack,
+    }
+  }
+})
+</script>
